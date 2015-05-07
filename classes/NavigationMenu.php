190936548530c04605f9231469be465b596515a1
Matthew Eggers <meggers@monsooncommerce.com>
2015-05-07 10:58:35 -0700

    SUP-6586 Honed recursive nav menu assignment

--- classes/NavigationMenu.php
+++ classes/NavigationMenu.php
@@ -6,37 +6,55 @@
  * @author Your Name <your.name at your.org>
  */
 class NavigationMenu {
-  public $name;
+  public $id;
   public $title;
   public $url;
+  public $parentId;
   public $children;
   
   public function __construct($menuItem, $chilren = null) {
-    $this->name = $menuItem->name;
+    $this->id = $menuItem->ID;
     $this->title = $menuItem->title;
     $this->url = $menuItem->url;
+    $this->parentId = $menuItem->menu_item_parent;
     $this->children = (is_null($chilren)) ? array() : $chilren;
   }
-  //Make recursive
+
   public static function setNavMenuArray($menuItemsArray) {
+    $baseArray = self::convertMenuItems($menuItemsArray);
     $returnArray = [];
-    foreach ($menuItemsArray as $menuItem) {
-       self::setChildrenOf($menuItem, $menuItemsArray);
+    foreach ($baseArray as $menuItem) {
+      self::setChildren($menuItem, $baseArray);
+      if ($menuItem->parentId == 0) {
+        $returnArray[] = $menuItem;
+      }
+      
     }
     return $returnArray;
   }
-  private static function setChildrenOf($parentMenuItem, $childArray) {
-    foreach($childArray as $childArray) {
-
-    }
-  }
-  private static function getChildrenOf($parentMenuItem, $menuItemsArray) {
+  
+  private static function getChildrenOf(NavigationMenu $parentMenuItem, $baseArray) {
     $childArray = [];
-    foreach($menuItemsArray as $menuItem) {
-      if ($menuItem->menu_item_parent == $parentMenuItem->ID) {  
+    foreach($baseArray as $menuItem) {
+      if ($menuItem->parentId == $parentMenuItem->id) {  
         $childArray[] = $menuItem;
       }
     }
     return $childArray;
+  }
+  
+  private static function setChildren(NavigationMenu $parent, $baseArray) {
+    $parent->children = self::getChildrenOf($parent, $baseArray);
+    foreach ($parent->children as $child) {
+      $child->children = self::setChildren($child, self::getChildrenOf($child, $baseArray), $baseArray);
+    }
+  }
+  
+  private static function convertMenuItems($menuItems) {
+    $returnArray = [];
+    foreach ($menuItems as $menuItem) {
+      $returnArray[] = new NavigationMenu($menuItem);
+    }
+    return $returnArray;
   }
 }
