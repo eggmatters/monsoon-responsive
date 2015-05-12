7f93158fd542e00ae4ff9fd34cf43093454ff76d
Matthew Eggers <meggers@monsooncommerce.com>
2015-05-12 12:28:11 -0700

    SUP-6594 Added search results page

---
+++ search.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for displaying search results pages.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
+get_header(); ?>
+
+	<div class="container">
+		<div class="row">
+		<?php 
+      if ( have_posts() ) : 
+        ?>
+
+			<header class="page-header">
+				<h3 class="page-title"><?php printf( __( 'Search Results for: %s', 'monsoon-responsive' ), get_search_query() ); ?></h3>
+			</header><!-- .page-header -->
+      <div class="col-md-8">
+      <?php
+			  // Start the loop.
+			  while ( have_posts() ) : the_post(); ?>
+        <div class="row">
+          <div class="col-md-12">
+            <a style="font-size: 18px;" class="large-text" href="<?php get_post_permalink()?>"><?php the_title()?></a><br>
+            <?php the_excerpt(); ?>
+          </div>
+        </div>
+        
+			<?php endwhile; ?>
+      </div>
+      
+      <div class="col-md-4">
+        <?php get_sidebar(); ?>
+      </div>
+      
+		<?php else :?>
+      <div class="row">
+        <?php get_template_part( 'content', 'none' ); ?>
+      </div>
+
+		<?php endif; ?>
+    </div>
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
