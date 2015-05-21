eba555aa94dd5dd1d661296bace0504faa7473a9
Matthew Eggers <meggers@monsooncommerce.com>
2015-05-21 15:02:10 -0700

    SUP-6644 Added quicktag for icon-banner

--- js/editorQuicktags.js
+++ js/editorQuicktags.js
@@ -1,59 +1,35 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* initial commit
+ * dfb252dd0d1b193584ec3d6a0b56f9e72021f216
+ * 
  */
 
-
 function generateBannerIcon(e, c, ed) {
-  var prmt, t = this;
-  if (ed.canvas.selectionStart !== ed.canvas.selectionEnd) {
-    // if we have a selection in the editor define out tagStart and tagEnd to wrap around the text
-    // prompt the user for the abbreviation and return gracefully on a null input
-    prmt = prompt('Enter Abbreviation');
-    if (prmt === null)
-      return;
-    t.tagStart = '<abbr title="' + prmt + '">';
-    t.tagEnd = '</abbr>';
-  } else if (ed.openTags) {
-    // if we have an open tag, see if it's ours
-    var ret = false, i = 0, t = this;
-    while (i < ed.openTags.length) {
-      ret = ed.openTags[i] == t.id ? i : false;
-      i++;
-    }
-    if (ret === false) {
-      // if the open tags don't include 'abbr' prompt for input
-      prmt = prompt('Enter Abbreviation');
-      if (prmt === null)
-        return;
-      t.tagStart = '<abbr title="' + prmt + '">';
-      t.tagEnd = false;
-      if (!ed.openTags) {
-        ed.openTags = [];
-      }
-      ed.openTags.push(t.id);
-      e.value = '/' + e.value;
-    } else {
-      // otherwise close the 'abbr' tag
-      ed.openTags.splice(ret, 1);
-      t.tagStart = '</abbr>';
-      e.value = t.display;
-    }
-  } else {
-    // last resort, no selection and no open tags
-    // so prompt for input and just open the tag
-    prmt = prompt('Enter Abbreviation');
-    if (prmt === null)
-      return;
-    t.tagStart = '<abbr title="' + prmt + '">';
-    t.tagEnd = false;
-    if (!ed.openTags) {
-      ed.openTags = [];
-    }
-    ed.openTags.push(t.id);
-    e.value = '/' + e.value;
+  var t = this;
+  var iconUrl = prompt("Enter Icon URL:", 'https://');
+  if (iconUrl === null) {
+    return;
   }
+  var altText = prompt("Enter alt text:");
+  if (altText === null) {
+    return;
+  }
+  var leader = prompt("Enter Banner text");
+  if (leader === null) {
+    return;
+  }
+  t.tagStart = '<div class="row">\n' 
+    +  '  <div class="col-md-2">\n'
+    +  '    <img class="img-responsive" src="' + iconUrl + '" alt="' + altText +'" />\n'
+    + '   </div>\n'
+    + '   <div class="col-md-10">\n'
+    + '     <div class="row"><p>&nbsp;</p></div>\n'
+    + '     <div class="row"> <p class="lead">' + leader + '</p> </div>\n'
+    + '     <div class="row"><p>&nbsp;</p></div>\n'
+    + '  </div>\n'
+    + '</div>\n'
+    + '<p>&nbsp;</p>';
+  t.tagEnd = '';
+  
   // now we've defined all the tagStart, tagEnd and openTags we process it all to the active window
   QTags.TagButton.prototype.callback.call(t, e, c, ed);
 }
