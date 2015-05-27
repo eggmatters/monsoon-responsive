08026b9879ac3cad3cd12bc0590e417acfef6ffe
Jennifer McVey <jmcvey@monsooncommerce.com>
2015-05-27 12:08:53 -0700

    removed old form, added btn hover class

--- footer.php
+++ footer.php
@@ -3,36 +3,18 @@
   <div class="container-fluid">
   	<div class="container" id="infoExch">
 	   <div class="row">
-	    
-	    <!-- trial form -->
+
 			<form class="form-horizontal">
 				<div class="form-group">
 					<label for="info-exchange-signup" class="control-label col-lg-3 medLgText">Subscribe to Info Exchange</label>
 					<div class="input-group col-lg-8">
 						<input type="text" id="info-exchange-signup" name="info-exchange-signup" class="form-control" placeholder="you@email.com">
 						<span class="input-group-btn">
-		            <button class="btn btn-default dkpurplebg btnTextWhite" type="button" data-toggle="modal" data-target="#mr-ix-signup"><b>SIGN UP NOW!</b></button>				
+		            <button class="btn btn-default dkpurplebg btnTextWhite btnTextWhite:hover" type="button" data-toggle="modal" data-target="#mr-ix-signup"><b>SIGN UP NOW!</b></button>				
 					</div>
 				</div>			
 			</form>	    	
-	    	
-	    	<!-- old form -->
-		    <div class="col-lg-3">
-		      <label class="pull-right">Subscribe to Info Exchange</label>
-		    </div>
-		    <div class="col-lg-8">
-		      <form style="padding-bottom: 2%">
-		        <div class="input-group">
-		          <input type="text" id="info-exchange-signup" name="info-exchange-signup" class="form-control" placeholder="you@email.com">
-		          <span class="input-group-btn">
-		            <button class="btn btn-default" type="button" data-toggle="modal" data-target="#mr-ix-signup">Sign up Now</button>
-		          </span>
-		        </div><!-- /input-group -->
-		      </form>
-		    </div>
-		    <div class="col-lg-1"></div>
-	    	
-	   
+	    		   
 	  </div><!-- end .row -->
   </div><!-- end .container #infoExch -->
     <div class="row">
