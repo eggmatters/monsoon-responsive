de4ea72f95606af41ff25b15528463128d2fa99a
Jennifer McVey <jmcvey@monsooncommerce.com>
2015-05-27 17:17:37 -0700

    feedback button

--- footer.php
+++ footer.php
@@ -1,3 +1,8 @@
+<?php
+$fb=get_template_directory() . '/images/icon-footer-facebook.png';
+
+?>
+
 
 <div class="jumbotron dkpurplebg" style="margin-bottom: 0px;">
   <div class="container-fluid">
@@ -10,42 +15,48 @@
 					<div class="input-group col-lg-8">
 						<input type="text" id="info-exchange-signup" name="info-exchange-signup" class="form-control" placeholder="you@email.com">
 						<span class="input-group-btn">
-		            <button class="btn btn-default dkpurplebg btnTextWhite btnTextWhite:hover" type="button" data-toggle="modal" data-target="#mr-ix-signup"><b>SIGN UP NOW!</b></button>				
+		            <button class="btn btn-default dkpurplebg textWhite textWhite:hover" type="button" data-toggle="modal" data-target="#mr-ix-signup"><b>SIGN UP NOW!</b></button>				
 					</div>
 				</div>			
 			</form>	    	
 	    		   
 	  </div><!-- end .row -->
   </div><!-- end .container #infoExch -->
-    <div class="row">
-      <div class="container">
-        <div class="col-md-4 text-center">
-          Pro Webhelp
-        </div>
-        <div class="col-md-4 text-center">
-          SEKB
-        </div>
-        <div class="col-md-4 text-center">
-          Solution Central
-        </div>
+
+    <div class="row topMargin60">
+      <div class="container"> 
+        <div class="col-md-3"></div>
+        <div class="col-md-2 text-right"><a href="#" class="textWhite textWhite:hover">Pro Webhelp</a></div>
+        <div class="col-md-2 text-center"><a href="#" class="textWhite textWhite:hover">SEKB</a></div>
+        <div class="col-md-2 text-left"><a href="#" class="textWhite textWhite:hover">Solution Central</a></div>
+        <div class="col-md-3"></div>
       </div>
     </div>
     <p>&nbsp;</p>
     <div class="row">
       <div class="container">
         <div class="col-md-12 text-center">
-          <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#mr-feedback">
-              Feedback
-          </button>
+          <button type="button" class="btn textBlack" id="bubble" data-toggle="modal" data-target="#mr-feedback">Feedback</button>
         </div>
       </div>
     </div>
-    <p>&nbsp;</p>
+
+	<div class="container">
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-2"><a href="https://www.facebook.com/monsooncommerce1"><img src="<?php echo $fb; ?>" alt="facebook"></a></div>
+        <div class="col-md-2"></div>
+        <div class="col-md-2"></div>
+        <div class="col-md-2"></div>
+        <div class="col-md-2"></div>
+      </div>
+    </div>    
+    
     <div class="container">
       <div class="row">
         <div class="col-md-4"></div>
         <div class="col-md-4 text-center">
-          <p>©2015 All rights reserved</p>
+          ©2015 All rights reserved
         </div>
         <div class="col-md-4"></div>
       </div>
