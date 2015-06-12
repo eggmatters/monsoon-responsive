e6403a7b31e6e897654982b3180ffed3f8bb5157
Matthew Eggers <meggers@monsooncommerce.com>
2015-06-12 09:52:15 -0700

    Added 404 template to page

---
+++ 404.php
@@ -0,0 +1,32 @@
+<?php
+get_header();
+?>
+<style>
+  .spacer-404 {
+    margin-bottom: 7.2%;
+  }
+</style>
+<div class="jumbotron dkpurplebg">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="container">
+        <div class="text-center topMargin60 xlgText"><?php _e( 'Oops! That page can&rsquo;t be found.', 'monsoon-responsive' ); ?></div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container-fluid">
+  <?php get_banner_search(); ?>
+</div>
+<p>&nbsp;</p>
+<div class="container textGreen">
+  <div class="row">
+    <div class="col-md-12">
+      <p>It is with great sadness that we now inform you that this page no longer exists.</p>
+      <p>There are plenty of great pages to choose from however. I'm sure you'll find just what you're looking for.</p>
+      <p>Please feel free to <a href="<?php echo get_site_url() . '/contact'; ?>">Contact Us</a> if you still can't find what you're looking for.</p>
+    </div>
+  </div>
+</div>
+<div class="spacer-404"></div>
+<?php get_footer();
