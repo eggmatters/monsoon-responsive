95fb3d32466dcf4cb7502931f1e4b9d50b613a04
Matthew Eggers <meggers@monsooncommerce.com>
2015-06-17 12:09:22 -0700

    removed addresses and maps from contact us

--- contact-us.php
+++ contact-us.php
@@ -15,59 +15,9 @@ get_header();
   <div class="row">
     <div class="col-lg-12 text-center">
       <h1 class="contact-h1">Contact Us</h1>
-      <p class="contact-paragraph">Reach out for sales, support, and more</p>
     </div>
   </div>
   <p>&nbsp;</p>
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-6">
-        <h3 class="contact-h3">GLOBAL CORPORATE HEADQUARTERS</h3>
-        <address class="contact-address">
-          520 NW Davis Street<br>
-          Suite 300<br>
-          Portland, OR 97209<br>
-          Phone: 503-239-1055<br>
-        </address>
-      </div>
-      <div class="col-lg-6">
-        <div class="map-container-6">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2795.2925766967446!2d-122.6758525!3d45.524317599999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54950a00e1c15323%3A0x7819ed5f19534639!2s520+NW+Davis+St!5e0!3m2!1sen!2sus!4v1433364831304" class="map-size-6" frameborder="0" style="border:0"></iframe>
-        </div>
-      </div>
-    </div>
-    <p>&nbsp;</p>
-    <div class="row">
-      <div class="col-lg-3">
-        <h3 class="contact-h3">EMERYVILLE OFFICE</h3>
-        <address class="contact-address">
-          1250 45th Street<br>
-          Suite 100<br>
-          Emeryville, CA 94608<br>
-          Phone: 510-594-4500
-        </address>
-      </div>
-      <div class="col-lg-3">
-        <div class="map-container-3">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.135200869485!2d-122.2844197!3d37.8337206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80857e6b010aec6f%3A0x335fb9073851af70!2s1250+45th+St%2C+Emeryville%2C+CA+94608!5e0!3m2!1sen!2sus!4v1433365536785" class="map-size-6" frameborder="0" style="border:0"></iframe>
-        </div>
-      </div>
-      <div class="col-lg-3">
-        <h3 class="contact-h3">PHILADELPHIA OFFICE</h3>
-        <address class="contact-address">
-          399 Arcola Road<br>
-          Suite 200<br>
-          Collegeville, PA 19426<br>
-          Phone: 484-927-4804<br>
-        </address>
-      </div>
-      <div class="col-lg-3">
-        <div class="map-container-3">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6098.77755047681!2d-75.46397124391754!3d40.15589691542109!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6911dbaef4761%3A0x2bb3c33faa1160c6!2s399+Arcola+Rd%2C+Collegeville%2C+PA+19426!5e0!3m2!1sen!2sus!4v1433365701692" class="map-size-6"frameborder="0" style="border:0"></iframe>
-        </div>
-      </div>
-    </div>
-  </div>
 </div>
 <div class="jumbotron noBottomMargin topMargin60 jumbofont">
   <div class="container" id="cu-form-container">
