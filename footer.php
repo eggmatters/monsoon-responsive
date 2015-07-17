4b08c0682c1138b157eca95948f054a62f4cb2e6
Matthew Eggers <meggers@monsooncommerce.com>
2015-07-17 10:15:37 -0700

    Updated footer links

--- footer.php
+++ footer.php
@@ -8,63 +8,46 @@ $li = get_mr_theme_root_uri() . '/images/icon-footer-linkedin.png';
 
 <div class="jumbotron dkpurplebg noBottomMargin">
   <div class="container-fluid">
-  	<div class="container" id="infoExch">
-	   <div class="row">
-
-			<form id="ie" class="form-horizontal">
-				<div id="ie" class="form-group">
-					<label for="info-exchange-signup" class="control-label col-xs-12 col-sm-5 col-md-4 medLgText">Subscribe to Info Exchange</label>
-					<div class="input-group col-xs-11 col-sm-6 col-md-8" >
-						<input type="text" id="info-exchange-signup-email" name="info-exchange-signup-email" class="form-control ie" placeholder="you@email.com">
-						<span class="input-group-btn">
-              <a class="btn btn-default ieBtn" href="http://eepurl.com/xpdf9" target="_blank"><b>SIGN UP NOW!</b></a>
-            </span>
-					</div>
-				</div>			
-			</form>    	
-	    		   
-	  </div><!-- end .row -->
-  </div><!-- end .container #infoExch -->
-	<div class="container"> 
+    <div class="container" id="infoExch">
       <div class="row">
-      
-        <div class="col-md-4 text-center topMargin60"><a href="http://webhelp.monsooncommerce.com/" target="_blank" class="textWhite foot">Marketplace Knowledge Base</a></div>
-        <div class="col-md-4 text-center topMargin60"><a href="http://www.stoneedge.com/help/" target="_blank" class="textWhite foot">OMS Knowledge Base</a></div>
-        <div class="col-md-4 text-center topMargin60"><a href="https://sc.monsooncommerce.com/" target="_blank" class="textWhite foot">Solution Central</a></div>
 
-      </div>
-    
+        <form id="ie" class="form-horizontal">
+          <div id="ie" class="form-group">
+            <label for="info-exchange-signup" class="control-label col-xs-12 col-sm-5 col-md-4 medLgText">Subscribe to Info Exchange</label>
+            <div class="input-group col-xs-11 col-sm-6 col-md-8" >
+              <input type="text" id="info-exchange-signup-email" name="info-exchange-signup-email" class="form-control ie" placeholder="you@email.com">
+              <span class="input-group-btn">
+                <a class="btn btn-default ieBtn" href="http://eepurl.com/xpdf9" target="_blank"><b>SIGN UP NOW!</b></a>
+              </span>
+            </div>
+          </div>			
+        </form>    	
+
+      </div><!-- end .row -->
+    </div><!-- end .container #infoExch -->
+    <div class="container"> 
       <div class="row">
-      
-        <div class="col-md-4 text-center topMargin10">&nbsp;</div>
-        <div class="col-md-4 text-center topMargin10"><a href="http://forum.stoneedge.com/" target="_blank" class="textWhite foot">OMS User Forums</a></div>
-        <div class="col-md-4 text-center topMargin10">&nbsp;</div>
-
-      </div>
-      <div class="row">
-      
-        <div class="col-md-4 text-center topMargin10">&nbsp;</div>
-        <div class="col-md-4 text-center topMargin10"><a href="http://209.235.247.148/dlgateway/Login.aspx?ReturnUrl=%2Fdlgateway%2Fdefault.aspx" target="_blank" class="textWhite foot">OMS Downloads</a></div>
-        <div class="col-md-4 text-center topMargin10">&nbsp;</div>
-
+        <div class="col-md-4 text-center footer-text-bold topMargin60"><a href="https://sc.monsooncommerce.com/" target="_blank" class="textWhite foot">SOLUTION CENTRAL</a></div>
+        <div class="col-md-4 text-center footer-text-bold topMargin60"><a href="https://www.monsooncommerce.com/" target="_blank" class="textWhite foot">MONSOON COMMERCE</a></div>
+        <div class="col-md-4 text-center footer-text-bold topMargin60"><a href="https://www.monsooncommerce.com/monsoon-commerce-app-store/" target="_blank" class="textWhite foot">APP STORE</a></div>
       </div>
     </div>
     <div class="container">
-	    <div class="row">
-	        <div class="col-md-12 text-center topMargin60">
-	          <button type="button" class="btn textBlack" id="bubble" data-toggle="modal" data-target="#mr-feedback">Share your feedback on the Support Center.</button>
-	        </div>
-	    </div>
-	    <div class="row topMargin60">
-		      <div class="col-xs-2 col-sm-2 col-md-4 col-lg-4"></div>
-		      <div class="col-xs-2 col-sm-2 col-md-1 col-lg-1"><a href="https://www.facebook.com/monsooncommerce1" target="_blank"><img src="<?php echo $fb; ?>" alt="facebook"></a></div>
-		      <div class="col-xs-2 col-sm-2 col-md-1 col-lg-1"><a href="https://www.twitter.com/monsooncommerce" target="_blank"><img src="<?php echo $tw; ?>" alt="twitter"></a></div>
-		      <div class="col-xs-2 col-sm-2 col-md-1 col-lg-1"><a href="https://www.youtube.com/user/MonsoonCommerce" target="_blank"><img src="<?php echo $yt; ?>" alt="youtube"></a></div>
-		      <div class="col-xs-2 col-sm-2 col-md-1 col-lg-1"><a href="https://www.linkedin.com/company/monsoon-commerce" target="_blank"><img src="<?php echo $li; ?>" alt="linkedin"></a></div>
-		      <div class="col-xs-2 col-sm-2 col-md-4 col-lg-4"></div>
-	    </div>
+      <div class="row">
+        <div class="col-md-12 text-center topMargin60">
+          <button type="button" class="btn textBlack" id="bubble" data-toggle="modal" data-target="#mr-feedback">Share your feedback on the Support Center.</button>
+        </div>
+      </div>
+      <div class="row topMargin60">
+        <div class="col-xs-2 col-sm-2 col-md-4 col-lg-4"></div>
+        <div class="col-xs-2 col-sm-2 col-md-1 col-lg-1"><a href="https://www.facebook.com/monsooncommerce1" target="_blank"><img src="<?php echo $fb; ?>" alt="facebook"></a></div>
+        <div class="col-xs-2 col-sm-2 col-md-1 col-lg-1"><a href="https://www.twitter.com/monsooncommerce" target="_blank"><img src="<?php echo $tw; ?>" alt="twitter"></a></div>
+        <div class="col-xs-2 col-sm-2 col-md-1 col-lg-1"><a href="https://www.youtube.com/user/MonsoonCommerce" target="_blank"><img src="<?php echo $yt; ?>" alt="youtube"></a></div>
+        <div class="col-xs-2 col-sm-2 col-md-1 col-lg-1"><a href="https://www.linkedin.com/company/monsoon-commerce" target="_blank"><img src="<?php echo $li; ?>" alt="linkedin"></a></div>
+        <div class="col-xs-2 col-sm-2 col-md-4 col-lg-4"></div>
+      </div>
     </div>    
-    
+
     <div class="container">
       <div class="row topMargin40">
         <div class="col-md-4"></div>
@@ -74,7 +57,7 @@ $li = get_mr_theme_root_uri() . '/images/icon-footer-linkedin.png';
         <div class="col-md-4"></div>
       </div>
     </div>
-    
+
   </div> <!-- /container-fluid -->
 </div> <!-- /jumbotron -->
 <?php wp_footer(); ?>
